2fa4bf218d661a4e3d473b79d043d8b88204eada
avniop <goswamipriya09@gmail.com>
2026-03-11 19:44:36 +0530

    Add files via upload

---
+++ FHO 3.py
@@ -0,0 +1,11 @@
+outputFile = open("codingal ubdated.txt","w")
+
+inputFile = open('Repeated.txt',"r")
+
+lines_seen_so_far = set()
+for line in inputFile:
+    if line not in lines_seen_so_far:
+        outputFile.write(line)
+        lines_seen_so_far.add(line)
+inputFile.close()
+outputFile.close()
